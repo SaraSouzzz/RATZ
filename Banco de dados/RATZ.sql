4cd18115d9dc9bf80ea105b3adf08eedff51180c
SaraSouzzz <sara.costa@sptech.school>
2022-11-23 11:18:34 -0300

    banco de dados

---
+++ Banco de dados/RATZ.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE ratz;
+USE ratz;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+    email VARCHAR(50),
+    senha CHAR(8)
+);
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE Quiz(
+idquiz int auto_increment primary key,
+pontuacao int,
+fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(idusuario)
+);
+
+-- alter table usuario add column sobrenome varchar(50);
+
+select * from usuario;
+select * from aviso;
+select * from quiz;
