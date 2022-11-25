b6628d4a18ce1f65e3f9b8de2d1bf1080c36c96e
SaraSouzzz <sara.costa@sptech.school>
2022-11-25 15:01:41 -0300

    Quiz

---
+++ Public/Dashboard/questions.js
@@ -0,0 +1,35 @@
+export default [
+    {
+      question: "Qual a medida miníma para dois ratos?",
+      answers: [
+        { option: "60x60x40", correct: true },
+        { option: "30x30x40", correct: false },
+        { option: "60x30x20", correct: false },
+      ],
+    },
+    {
+      question: "Qual o tipo de forração usar?",
+      answers: [
+        { option: "Aspen", correct: true },
+        { option: "Areia de gato", correct: true },
+        { option: "Serragem", correct: false },
+        { option: "Areia de celulose", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 10 x 8?",
+      answers: [
+        { option: "800", correct: false },
+        { option: "90", correct: false },
+        { option: "80", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+      ],
+    },
+  ];
